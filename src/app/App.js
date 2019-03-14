fd4b56b827057bb154479bfbb4482cae46785670
franzp <pobocekfrantisek@gmail.com>
2019-03-14 12:17:59 +0100

    #4

---
+++ src/app/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import classes from './App.module.scss';
+
+class App extends Component {
+  render() {
+    return <div>app</div>;
+  }
+}
+
+export default App;
