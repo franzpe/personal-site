8ac6d729b7d5c5a276a6128843bfeebffce46bc3
franzp <pobocekfrantisek@gmail.com>
2019-03-17 16:22:31 +0100

    Menu trigger

--- src/app/Menu.js
+++ src/app/Menu.js
@@ -1,20 +1,38 @@
-import React from 'react';
+import React, { Fragment, useState, useContext } from 'react';
 import { NavLink } from 'react-router-dom';
+import cs from 'classnames';
 
 import classes from './Menu.module.scss';
+import { AppContext } from './AppContextProvider';
 
 function Menu() {
+  const [isMenuOpened, setIsMenuOpened] = useState(false);
+  const context = useContext(AppContext);
+
   return (
-    <nav className={classes.menu}>
-      <ul className={classes.inner}>
-        <li>
-          <NavLink to="/about">About</NavLink>
-        </li>
-        <li>
-          <NavLink to="/posts">Posts</NavLink>
-        </li>
-      </ul>
-    </nav>
+    <Fragment>
+      <nav
+        className={cs(classes.menu, {
+          [classes.darkTheme]: context.theme.currTheme === 'dark',
+          ['hidden']: isMenuOpened
+        })}
+      >
+        <ul className={classes.inner}>
+          <li>
+            <NavLink to="/about">About</NavLink>
+          </li>
+          <li>
+            <NavLink to="/posts">Projects</NavLink>
+          </li>
+        </ul>
+      </nav>
+      <span className={classes.trigger} onClick={() => setIsMenuOpened(!isMenuOpened)}>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+          <path d="M0 0h24v24H0z" fill="none" />
+          <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" />
+        </svg>
+      </span>
+    </Fragment>
   );
 }
 
