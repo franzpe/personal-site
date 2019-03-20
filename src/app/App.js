deb3f703600c8428916d8cfa9832528111daeec6
franzp <pobocekfrantisek@gmail.com>
2019-03-20 21:46:02 +0100

    Google analytics

--- src/app/App.js
+++ src/app/App.js
@@ -1,4 +1,5 @@
 import React, { Component, Fragment } from 'react';
+import ReactGA from 'react-ga';
 
 import Head from './Head';
 import classes from './App.module.scss';
@@ -6,13 +7,27 @@ import Routes from './Routes';
 import Footer from './Footer';
 import Header from './Header';
 import { AppContext } from './AppContextProvider';
+import history from '../_utils/history';
 
 class App extends Component {
   static contextType = AppContext;
 
   componentDidMount = () => {
+    this.setupGoogleAnalytics();
+  };
+
+  componentWillMount() {
     const { theme } = this.context;
     theme.init();
+  }
+
+  setupGoogleAnalytics = () => {
+    ReactGA.initialize('UA-136592703-1');
+    ReactGA.pageview(history.location.pathname + history.location.search);
+
+    history.listen((location, action) => {
+      ReactGA.pageview(location.pathname + location.search);
+    });
   };
 
   render() {
--- src/index.js
+++ src/index.js
@@ -16,4 +16,4 @@ ReactDOM.render(
   </Router>,
   document.getElementById('root')
 );
-serviceWorker.register();
+serviceWorker.unregister();
