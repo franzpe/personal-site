e219575f352db3a8a6ff76a0c2236abad15a8276
franzp <pobocekfrantisek@gmail.com>
2019-03-20 21:45:42 +0100

    Firefox optimalization

--- src/app/Header.js
+++ src/app/Header.js
@@ -17,12 +17,6 @@ function Header() {
         <Logo />
         <span className={classes.right}>
           <Menu />
-          <span className="menu-trigger">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-              <path d="M0 0h24v24H0z" fill="none" />
-              <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" />
-            </svg>
-          </span>
           <span className={classes.themeToggle} onClick={() => context.theme.toggle()}>
             <svg
               className={classes.themeToggler}
